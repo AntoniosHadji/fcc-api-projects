d0569ef02a0912b51d269060124d2756ba62680e
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2017-01-26 17:40:30 -0500

    set up menu

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index.js');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,11 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const index = require('./routes/index.js');
 app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -44,23 +43,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
 // Get port from environment and store in Express.
 app.set('port', (process.env.PORT || '3000'));
 
-/**
- * Create HTTP server.
- */
-var debug = require('debug')('http');
-var http = require('http');
-var server = http.createServer(app);
-/**
- * Listen on provided port, on all network interfaces.
- */
+const debug = require('debug')('app');
 
-server.listen(app.get('port'));
-server.on('listening', onListen);
-server.on('error', onError);
+app.listen(app.get('port'));
+app.on('listening', onListen);
+app.on('error', onError);
 
 function onError(error) {
   console.error(error);

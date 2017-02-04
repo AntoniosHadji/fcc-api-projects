2aa1f9c7d15d750667210b16c3050d5b62b58d63
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2017-02-04 17:01:04 -0500

    file meta merged

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Get port from environment and store in Express.
+app.set('port', (process.env.PORT || 3000));
+// set database uri
+app.set('dburi', process.env.MONGODB_URI);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 // add this line to register partial directory in handlebars
 require('hbs').registerPartials(path.join(__dirname, 'views', 'partial'));
 
@@ -42,9 +46,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// Get port from environment and store in Express.
-app.set('port', (process.env.PORT || '3000'));
 
 const debug = require('debug')('app');
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+// required for the get-meta page
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'FCC Backend API Projects' });
+});
+
+router.get('/file-metadata', (req, res, next) => {
+  res.render('index-filemeta', { title: 'FCC File MetaData' });
+});
+
+/* GET META page. */
+router.post('/file-metadata/get-meta', upload.single('testfile'),
+    (req, res, next) => {
+  let metaData = {};
+  metaData.size = req.file.size;
+  metaData.name = req.file.originalname;
+  metaData.type = req.file.mimetype;
+  res.json(metaData);
 });
 
 module.exports = router;

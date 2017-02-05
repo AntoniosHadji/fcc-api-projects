73965799ceb3c3b35e06d2ec53bb6be2beb0a2c1
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2017-02-05 12:40:00 -0500

    complete merging of all projects

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+'use strict'
+if ((process.env.NODE_ENV || 'development') === 'development') {
+  require('dotenv').config();
+}
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongo = require('mongodb').MongoClient
 
 const app = express();
 
@@ -18,7 +23,6 @@ app.set('view engine', 'hbs');
 // add this line to register partial directory in handlebars
 require('hbs').registerPartials(path.join(__dirname, 'views', 'partial'));
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -26,39 +30,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const index = require('./routes/index.js');
-app.use('/', index);
+mongo.connect(app.get('dburi'), (dberr, database) => {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  if (dberr) {
+    console.error(dberr);
+  } else {
+    console.log('Database connected');
+  }
+
+  app.use(function(req, res, next) {
+    req.db = database;
+    next();
+  });
+
+  app.use('/', require('./routes/index.js'));
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  const debug = require('debug')('app');
+
+  app.listen(app.get('port'));
+  app.on('listening', onListen);
+  app.on('error', onError);
+
+  function onError(error) {
+    console.error(error);
+  }
+
+  function onListen() {
+    debug('listening on ' + app.get('port'));
+  }
+
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-const debug = require('debug')('app');
-
-app.listen(app.get('port'));
-app.on('listening', onListen);
-app.on('error', onError);
-
-function onError(error) {
-  console.error(error);
-}
-
-function onListen() {
-  debug('listening on ' + app.get('port'));
-}
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,14 @@ const router = express.Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 // required for header data parser
 const extractHeaderData = require('../lib/extractHeaderData.js');
+// required for image search
+const getJSON = require('../lib/getJSON.js');
+const cleanData = require('../lib/cleanData.js');
+// required for url shortener
+const validate = require('valid-url');
+const jhash = require('jhash');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -42,7 +47,7 @@ router.get('/header-parser/json', (req, res, next) => {
 });
 
 /* timestamp microservice */
-router.get('/timestamp', function(req, res) {
+router.get('/timestamp', (req, res) => {
   let d = new Date();
   res.render('index-timestamp', {
     url1: '//'+req.get('Host')+'/timestamp/'+d.toDateString(),
@@ -52,7 +57,7 @@ router.get('/timestamp', function(req, res) {
   });
 });
 
-router.get('/timestamp/:data', function(req, res) {
+router.get('/timestamp/:data', (req, res) => {
   if (isNaN(Number.parseInt(req.params.data))) {
     if (isNaN(Date.parse(req.params.data))) {
       // neither date nor number parsed
@@ -69,6 +74,116 @@ router.get('/timestamp/:data', function(req, res) {
   }
 });
 
+/* image search */
+router.get('/image-search', (req, res, next) => {
+  res.render('index-imagesearch', { title: 'FCC Image Search Abstraction' });
+});
+
+router.get('/image-search/api', (req, res, next) => {
+  if (req.query.hasOwnProperty('q')) {
+    try {
+      req.db.collection('searchTerms').insertOne(
+          { term: req.query.q,
+            date: new Date()
+          }, (err) => {
+            // result omitted from function signature, not needed
+            if (err) console.error(err);
+          });
+    } catch (e) {
+      console.error(e);
+    }
+
+    let apiquery = 'https://www.googleapis.com/customsearch/v1?';
+    apiquery += `key=${process.env.GOOGLE_CSE_APIKEY}&`;
+    apiquery += `cx=${process.env.GOOGLE_CSE_CX}&`;
+    apiquery += `searchType=image&q=${req.query.q}`;
+
+    if (req.query.offset) {
+      apiquery += `&start=${Math.min((req.query.offset - 1) * 10 + 1, 91)}`;
+    }
+
+    getJSON(apiquery, (answer) => {
+      res.json(cleanData(answer.items));
+    });
+  } else {
+    res.status(500).json({error: 'Must supply a query to get results'});
+  }
+});
+
+/* /history */
+router.get('/image-search/history', (req, res, next) => {
+  try {
+    // show last 10 items
+    req.db.collection('searchTerms').find()
+      .sort({date: -1})
+      .limit(10)
+      .toArray( (err, docs) => {
+        if (err) res.json(err);
+        res.json(docs);
+      });
+  } catch (e) {
+    next(e)
+  }
+});
+
+/* url shortener */
+router.get('/url-short', function(req, res, next) {
+  res.render('index-url',
+      {
+        title: 'FCC URL Shortener Microservice',
+        hostname: req.protocol + '://' + req.hostname
+      }
+  );
+});
+
+
+router.get('/url-short/v1', function(req, res, next) {
+
+  let validUrl = validate.isWebUri(req.query.longUrl);
+
+  if (validUrl === undefined) {
+    res.json({error: 'url is invalid', status: 400});
+  } else {
+    let hashUrl = jhash.hash(validUrl);
+    try {
+    req.db.collection('urlmap')
+      .updateOne(
+          { shortUrl: hashUrl},
+          { shortUrl: hashUrl, longUrl: validUrl },
+          { upsert: true },
+        function(err, result) {
+          if (err) console.error(err);
+          console.log(`Inserted ${result.upsertedCount} document`);
+        });
+    } catch (tce) {
+      console.error(tce);
+    }
+
+    res.json({ shortUrl: hashUrl, longUrl: validUrl, status: 200});
+  }
+});
+
+/* GET short url and redirect. */
+router.get('/url-short/*', function(req, res, next) {
+
+  // req.originalUrl is the path with preceding / (no host)
+  // retriev final path which is only shortUrl identifier
+  let shortUrl = req.originalUrl.split('/');
+  shortUrl = shortUrl[shortUrl.length - 1];
+
+  try {
+    req.db.collection('urlmap')
+      .findOne({shortUrl: shortUrl}, function(err, result) {
+        if (err) console.error('error:', err);
+        if (result.hasOwnProperty('longUrl')) {
+          res.redirect(301, result.longUrl);
+        }
+    });
+  } catch (tcErr) {
+    console.error(tcErr);
+    next(tcErr);
+  }
+});
 
 
 module.exports = router;

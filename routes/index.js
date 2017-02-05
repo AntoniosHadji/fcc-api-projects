61bc860acfa0218ee62f45d0da416d323eb27bb0
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2017-02-05 10:51:57 -0500

    header parser integrated

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // required for the get-meta page
-var multer = require('multer');
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// required for header data parser
+const extractHeaderData = require('../lib/extractHeaderData.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'FCC Backend API Projects' });
 });
 
+/* GET file meta data api server */
 router.get('/file-metadata', (req, res, next) => {
   res.render('index-filemeta', { title: 'FCC File MetaData' });
 });
@@ -26,5 +30,16 @@ router.post('/file-metadata/get-meta', upload.single('testfile'),
 });
 
 /* header parser */
+router.get('/header-parser', (req, res, next) => {
+  let d = extractHeaderData(req);
+  d.title = 'FCC Request Header Parser on Express';
+  d.url = '//' + req.get('host');
+  res.render('index-headerparser', d);
+});
+
+router.get('/header-parser/json', (req, res, next) => {
+  res.jsonp(extractHeaderData(req));
+});
+
 
 module.exports = router;

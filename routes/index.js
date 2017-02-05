5aa49bc5fb33ab601776620fe1f051a920ff2069
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2017-02-05 11:18:10 -0500

    timestamp merged

--- routes/index.js
+++ routes/index.js
@@ -41,5 +41,34 @@ router.get('/header-parser/json', (req, res, next) => {
   res.jsonp(extractHeaderData(req));
 });
 
+/* timestamp microservice */
+router.get('/timestamp', function(req, res) {
+  let d = new Date();
+  res.render('index-timestamp', {
+    url1: '//'+req.get('Host')+'/timestamp/'+d.toDateString(),
+    url2: '//'+req.get('Host')+'/timestamp/'+Math.floor(d.getTime()/1000),
+    unix: Math.floor(d.getTime()/1000),
+    natural: d.toDateString(),
+  });
+});
+
+router.get('/timestamp/:data', function(req, res) {
+  if (isNaN(Number.parseInt(req.params.data))) {
+    if (isNaN(Date.parse(req.params.data))) {
+      // neither date nor number parsed
+      res.json({'unix': null, 'natural': null});
+    } else {
+      // date parsed
+      let d = new Date(req.params.data);
+      res.json({'unix': d.getTime()/1000, 'natural': d.toDateString()});
+    }
+  } else {
+    // number parsed
+    let n = new Date(req.params.data * 1000);
+    res.json({'unix': req.params.data, 'natural': n.toDateString()});
+  }
+});
+
+
 
 module.exports = router;

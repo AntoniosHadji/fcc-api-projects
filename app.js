93165a94e684ca7a2db2c77572dd0131555f7c3a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-26 10:43:44 -0400

    updated formatting to standardjs

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
 'use strict'
 if ((process.env.NODE_ENV || 'development') === 'development') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 const mongo = require('mongodb').MongoClient
 
-const app = express();
+const app = express()
 
 // Get port from environment and store in Express.
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3000))
 // set database uri
-app.set('dburi', process.env.MONGODB_URI);
+app.set('dburi', process.env.MONGODB_URI)
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 // add this line to register partial directory in handlebars
-require('hbs').registerPartials(path.join(__dirname, 'views', 'partial'));
+require('hbs').registerPartials(path.join(__dirname, 'views', 'partial'))
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 mongo.connect(app.get('dburi'), (dberr, database) => {
-
   if (dberr) {
-    console.error(dberr);
+    console.error(dberr)
   } else {
-    console.log('Database connected');
+    console.log('Database connected')
   }
 
-  app.use(function(req, res, next) {
-    req.db = database;
-    next();
-  });
+  app.use(function (req, res, next) {
+    req.db = database
+    next()
+  })
 
-  app.use('/', require('./routes/index.js'));
+  app.use('/', require('./routes/index.js'))
 
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+  app.use(function (req, res, next) {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
 
   // error handler
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
     // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+    res.status(err.status || 500)
+    res.render('error')
+  })
 
-  const debug = require('debug')('app');
+  const debug = require('debug')('app')
 
-  app.listen(app.get('port'));
-  app.on('listening', onListen);
-  app.on('error', onError);
+  app.listen(app.get('port'))
+  app.on('listening', onListen)
+  app.on('error', onError)
 
-  function onError(error) {
-    console.error(error);
+  function onError (error) {
+    console.error(error)
   }
 
-  function onListen() {
-    debug('listening on ' + app.get('port'));
+  function onListen () {
+    debug('listening on ' + app.get('port'))
   }
-
-});
-module.exports = app;
+})
+module.exports = app
